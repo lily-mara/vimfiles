ba35987653fca0b121d0e4f02d20f9bb33a6294f
unknown <natemara@gmail.com>
2013-11-22 00:34:23 +0000

    added easymotion plugin

--- bundle/git-repos.sh
+++ bundle/git-repos.sh
@@ -3,3 +3,4 @@ git clone https://github.com/sophacles/vim-processing.git
 git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/Lokaltog/vim-easymotion.git
